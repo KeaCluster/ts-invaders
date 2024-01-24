97d445f6448565251ebc784903ab124532331610
KeaCluster <zzabdiel29@gmail.com>
2024-01-24 16:54:46 -0600

    What are these methods

--- src/models/Bullet.ts
+++ src/models/Bullet.ts
@@ -2,15 +2,24 @@ export default class Bullet {
   private readonly width: number = 5;
   private readonly height: number = 20;
 
+  private _x: number;
+  private _y: number;
+
   constructor(
-    private x: number,
-    private y: number,
+    x: number,
+    y: number,
     private velocity: number,
     private bulletColor: string
-  ) { }
+  ) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x(): number { return this._x }
+  get y(): number { return this._y }
 
   draw(ctx: CanvasRenderingContext2D): void {
-    this.y -= this.velocity;
+    this._y -= this.velocity;
     ctx.fillStyle = this.bulletColor;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
