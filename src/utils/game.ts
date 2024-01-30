a1c16d0f3153d70afe931f082df2c397a73357d7
KeaCluster <zzabdiel29@gmail.com>
2024-01-29 23:17:46 -0600

    Cleanup
    Further functional cleanup i think

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,58 +1,66 @@
+import { GameState } from "../models/GameState";
 import BulletController from "../controllers/BulletController";
 import EnemyController from "../controllers/EnemyController";
-import Bullet from "../models/Bullet";
 import Player from "../models/Player";
 
 const background = new Image();
 background.src = '/src/assets/images/background.jpg';
 
-let isBackgroundLoaded = false;
-background.onload = () => isBackgroundLoaded = true;
+export const startGame = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D): void => {
+  const gameState = new GameState(canvas, ctx);
 
+  loadAssets().then(() => {
+    const playerBulletController = new BulletController(gameState.canvas, 10, '#d0620e', true);
+    const enemyBulletController = new BulletController(gameState.canvas, 4, 'white', false);
+    const enemyController = new EnemyController(gameState.canvas, enemyBulletController, playerBulletController);
+    const player = new Player(gameState.canvas, 3, playerBulletController);
 
-let isGameOver: boolean = false;
-let didWin: boolean = false;
+    gameState.reset();
 
-export const game = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
-
-  const playerBulletController = new BulletController(canvas, 10, '#d0620e', true);
-  const enemyBulletController = new BulletController(canvas, 4, 'white', false);
-  const enemyController = new EnemyController(canvas, enemyBulletController, playerBulletController);
-  const player = new Player(canvas, 3, playerBulletController);
-
-  checkGameOver(canvas, player, enemyBulletController, enemyController);
-  if (background) {
-    ctx.drawImage(background, 0, 0, ctx.canvas.width, ctx.canvas.height);
-  }
-
-  displayGameOver(canvas);
-  if (!isGameOver) {
-    enemyController.draw(ctx);
-    player.draw(ctx);
-    playerBulletController.draw(ctx);
-    enemyBulletController.draw(ctx);
-  }
-}
-const displayGameOver = (canvas: HTMLCanvasElement): void => {
-  if (isGameOver) {
-    let text: string = didWin ? "You Win" : "Game Over";
-    let textOffset: number = didWin ? 3.5 : 5;
-    ctx.fillStyle = "white"
-    ctx.font = "70px Arial";
-    ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
-  }
+    const gameLoop = () => {
+      if (!gameState.isGameOver) {
+        updateGame(gameState, player, enemyController, playerBulletController, enemyBulletController);
+        renderGame(ctx, gameState, player, enemyController, playerBulletController, enemyBulletController);
+        requestAnimationFrame(gameLoop);
+      } else {
+        displayGameOver(ctx, gameState);
+      }
+    }
+  })
 }
 
-const checkGameOver = (
-  canvas: HTMLCanvasElement,
+const loadAssets = async (): Promise<void> => {
+  const backgroundLoaded = new Promise<void>(_ => background.onload = () => _());
+  await backgroundLoaded;
+}
+function updateGame(gameState: GameState, player: Player, enemyController: EnemyController, playerBulletController: BulletController, enemyBulletController: BulletController) {
+  // Logic to update game entities and check for game over conditions
+  // This might include moving entities, checking for collisions, etc.
+}
+
+
+const renderGame = (
+  ctx: CanvasRenderingContext2D,
+  gameState: GameState,
   player: Player,
-  enemyBulletController: BulletController,
   enemyController: EnemyController,
-): boolean => {
-  if (isGameOver) return;
-  if (enemyBulletController.collideWith(player)) { isGameOver = true; };
-  if (enemyController.collideWith(player)) { isGameOver = true };
-  if (enemyController.enemyRows.length === 0) {
-    didWin = true; isGameOver = true;
+  playerBulletController: BulletController,
+  enemyBulletController: BulletController) => {
+  if (background.complete) {
+    ctx.drawImage(background, 0, 0, gameState.canvas.width, gameState.canvas.height);
   }
+
+  enemyController.draw(ctx);
+  player.draw(ctx);
+  playerBulletController.draw(ctx);
+  enemyBulletController.draw(ctx);
+}
+
+const displayGameOver = (ctx: CanvasRenderingContext2D, gameState: GameState) => {
+  let text = gameState.didWin ? "You Win" : "Game Over";
+  let textOffset = gameState.didWin ? 3.5 : 5;
+
+  ctx.fillStyle = "white"
+  ctx.font = "70px Arial";
+  ctx.fillText(text, gameState.canvas.width / textOffset, gameState.canvas.height / 2);
 }
